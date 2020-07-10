1ea74fa93f294c607f010b6b8cd919aaf49c5056
Jin Choi <jinchoi@ucdavis.edu>
2020-07-09 19:37:06 -0700

    added comments read and create

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+
+const app = express()
+
+const commentsByPostId = {}
+
+app.use(express.json())
+
+app.get('/posts/:postId/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.postId])
+})
+
+app.post('/posts/:postId/comments', (req, res) => {
+  const id = randomBytes(4).toString('hex')
+  const { content } = req.body
+  const { postId } = req.params
+
+  if (!commentsByPostId[postId]) {
+    commentsByPostId[postId] = []
+  }
+
+  commentsByPostId[postId].push({
+    id, content
+  })
+
+  res.status(201).send(commentsByPostId[postId])
+})
+
+app.listen(3002, () => { console.log('Comments server on 3002') })
