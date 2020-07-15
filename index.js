6f633a5e27f270ae2183fa03366df7ab502360b7
root <root@DESKTOP-AUM85MI.localdomain>
2020-07-15 14:18:08 -0700

    added status to comments

--- index.js
+++ index.js
@@ -23,15 +23,18 @@ app.post('/posts/:postId/comments', (req, res) => {
     commentsByPostId[postId] = []
   }
 
-  commentsByPostId[postId].push({
-    id, content
-  })
+  const newComment = {
+    id,
+    content,
+    status: 'pending'
+  }
+
+  commentsByPostId[postId].push(newComment)
 
   axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
-      id,
-      content,
+      ...newComment,
       postId
     }
   })
@@ -39,8 +42,27 @@ app.post('/posts/:postId/comments', (req, res) => {
   res.status(201).send(commentsByPostId[postId])
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Event Received: ', req.body.type)
+
+  const { type, data } = req.body
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status } = data
+
+    const comments = commentsByPostId[postId]
+    const comment = comments.find(comment => comment.id === id)
+
+    comment.status = status
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        ...comment,
+        postId
+      }
+    })
+  }
 
   res.send()
 })
